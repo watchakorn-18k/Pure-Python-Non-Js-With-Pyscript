d695f7c4f7d757081f14b75fa57828c77468b86e
watchakorn-18k <porton2559@gmail.com>
2022-10-28 23:01:02 +0700

    Fix Clock small

--- image_upload.py
+++ image_upload.py
@@ -21,6 +21,11 @@ sec = js.document.getElementById("second")
 minte = js.document.getElementById("minute")
 hour = js.document.getElementById("hour")
 
+def go_to_source(e):
+    js.window.location = 'image_upload.py'
+
+btn_source_code = js.document.getElementById("btn_source_code").addEventListener('click',pyodide.ffi.create_proxy(go_to_source))
+
 
 async def run():
     while True:
