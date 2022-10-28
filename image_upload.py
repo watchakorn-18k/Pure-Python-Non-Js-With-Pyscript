f36a0eca10c3bc3c6b11239790c542d14e9341f8
watchakorn-18k <porton2559@gmail.com>
2022-10-28 22:56:10 +0700

    Fix Clock small

--- image_upload.py
+++ image_upload.py
@@ -11,25 +11,21 @@ menu()
 timeDisplay = js.document.getElementById("time");
 
 def refreshTime() -> str:
-    D = datetime.now().strftime('%d')
     H = datetime.now().strftime('%H')
     M = datetime.now().strftime('%M')
     S = datetime.now().strftime('%S')
 
-    return H,M,S,D
+    return H,M,S
 
 sec = js.document.getElementById("second")
 minte = js.document.getElementById("minute")
 hour = js.document.getElementById("hour")
-day = js.document.getElementById("day")
 
 
 async def run():
     while True:
         await asyncio.sleep(1)
-        H,M,S,D = refreshTime()
-        print(D)
-        day.style = f"--value:{D}"
+        H,M,S = refreshTime()
         hour.style = f"--value:{H}"
         minte.style = f"--value:{M}"
         sec.style = f"--value:{S}"
--- layout.py
+++ layout.py
@@ -5,5 +5,5 @@ import pyodide
 menu_all = js.document.getElementById("menu_all")
 def menu():
     menu_all.innerHTML += "<li class='menu-title'><span>Example List</span></li>"
-    menu_all.innerHTML += "<li><a class='active' href='input_app.html'>Input App</a></li>"
+    menu_all.innerHTML += "<li><a href='input_app.html'>Input App</a></li>"
     menu_all.innerHTML += "<li><a href='image_upload.html'>Clock</a></li>"
